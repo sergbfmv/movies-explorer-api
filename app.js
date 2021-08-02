9d034952851ca15ad6d80188544f41d9d2b9b73e
Sergey Ignatov <sergbfmv@yandex.ru>
2021-08-02 18:16:24 +0300

    cors

--- app.js
+++ app.js
@@ -10,7 +10,21 @@ const { PORT = 3000 } = process.env;
 const routers = require('./routes/index');
 const { headError } = require('./middlewares/headError');
 
+const allowedCors = [
+  'https:////api.serg-movies.diploma.nomoredomains.monster',
+  'http:////api.serg-movies.diploma.nomoredomains.monster',
+  'localhost:3000',
+];
+
 const app = express();
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+});
 
 app.use(requestLogger);
 
